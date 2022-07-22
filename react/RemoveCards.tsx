f442becae45e4d4264911b155d9db68d59d153bf
eduardoGChavez <druidasco@gmail.com>
2022-07-22 11:42:15 -0500

    [ADD] Remove cards Personajes

--- react/RemoveCards.tsx
+++ react/RemoveCards.tsx
@@ -6,24 +6,19 @@ const RemoveCards = () => {
     });
 
     const remove = () => {
-        
-        // setTimeout(function() {
-            let nombres;
-            nombres = document.querySelectorAll('.vtex-rich-text-0-x-heading--persoanje-indiv');
-            console.log("---------------------------------------------------------------------------------------")
-            for( let i = 0; i < nombres.length; i++ ) {
-                console.log(nombres.length);
-                if( nombres[i].innerHTML === "Nombre" ) {
-                    nombres[i].parentNode?.parentNode?.parentNode?.parentNode?.parentElement?.remove();
-                }
-                else {
-                    let x = nombres[i].parentNode?.parentNode?.parentNode?.parentNode?.parentElement as HTMLElement;
-                    x.style.display = "block";
-                }
+        let nombres;
+        nombres = document.querySelectorAll('.vtex-rich-text-0-x-heading--persoanje-indiv');
+        console.log("---------------------------------------------------------------------------------------")
+        for( let i = 0; i < nombres.length; i++ ) {
+            console.log(nombres.length);
+            if( nombres[i].innerHTML === "Nombre" ) {
+                nombres[i].parentNode?.parentNode?.parentNode?.parentNode?.parentElement?.remove();
             }
-
-            console.log("---------------------------------------------------------------------------------------")
-        // }, 5000);
+            else {
+                let x = nombres[i].parentNode?.parentNode?.parentNode?.parentNode?.parentElement as HTMLElement;
+                x.style.display = "block";
+            }
+        }
     }
 
     return (
